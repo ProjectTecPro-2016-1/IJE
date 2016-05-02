5e827bfc4a33c44276b5025e296fff54d812f1c2
Dib <ceciliabessa@hotmail.com>
2016-05-02 10:46:08 -0300

    Identation and spacing in Camera class

--- engine/src/camera.cpp
+++ engine/src/camera.cpp
@@ -7,46 +7,47 @@
  */
 #include "core/camera.h"
 #include "core/rect.h"
-
 #include <iostream>
+
 using namespace std;
 
-class Camera::Impl
-{
+class Camera::Impl {
 public:
-    Impl(Camera *camera, Mode mode)
+    Impl(Camera * camera, Mode mode)
         : m_camera(camera), m_mode(mode), m_target(nullptr),
-        m_limits(-1000000, -1000000, 2000000, 2000000) {}
+        m_limits(-1000000, -1000000, 2000000, 2000000) {
+        }
 
-    Mode mode() const { return m_mode; }
-    void set_mode(Mode mode) { m_mode = mode; }
+    Mode mode() const {
+        return m_mode;
+    }
 
-    void follow(const Object *target) { m_target = target; }
+    void set_mode(Mode mode) {
+        m_mode = mode;
+    }
 
-    void update_self(unsigned long)
-    {
-        if (m_mode == Camera::FOLLOWING and m_target)
-        {
-            double x = m_target->x() + (m_target->w() - m_camera->w())/2;
-            double y = m_target->y() + (m_target->h() - m_camera->h())/2;
+    void follow(const Object * target) {
+        m_target = target;
+    }
 
-            if (x < m_limits.x())
-            {
+    void update_self(unsigned long) {
+        if (m_mode == Camera::FOLLOWING and m_target) {
+            double x = m_target->x() + (m_target->w() - m_camera->w()) / 2;
+            double y = m_target->y() + (m_target->h() - m_camera->h()) / 2;
+
+            if (x < m_limits.x()) {
                 x = m_limits.x();
             }
 
-            if (x + m_camera->w() > m_limits.x() + m_limits.w())
-            {
+            if (x + m_camera->w() > m_limits.x() + m_limits.w()) {
                 x = m_limits.x() + m_limits.w() - m_camera->w();
             }
 
-            if (y < m_limits.y())
-            {
+            if (y < m_limits.y()) {
                 y = m_limits.y();
             }
 
-            if (y + m_camera->h() > m_limits.y() + m_limits.h())
-            {
+            if (y + m_camera->h() > m_limits.y() + m_limits.h()) {
                 y = m_limits.y() + m_limits.h() - m_camera->h();
             }
 
@@ -54,61 +55,47 @@ public:
         }
     }
 
-    void draw_self() {}
+    void draw_self() {
+    }
 
-    void set_limits(const Rect& limits)
-    {
+    void set_limits(const Rect& limits) {
         m_limits = limits;
     }
 
 private:
-    Camera *m_camera;
+    Camera * m_camera;
     Mode m_mode;
-    const Object *m_target;
+    const Object * m_target;
     Rect m_limits;
 };
 
 Camera::Camera(double x, double y, double w, double h, Mode mode)
-    : Object(nullptr, "", x, y, w, h), m_impl(new Camera::Impl(this, mode))
-{
+    : Object(nullptr, "", x, y, w, h), m_impl(new Camera::Impl(this, mode)) {
 }
 
-Camera::~Camera()
-{
+Camera::~Camera() {
 }
 
-Camera::Mode
-Camera::mode() const
-{
+Camera::Mode Camera::mode() const {
     return m_impl->mode();
 }
 
-void
-Camera::set_mode(Mode mode)
-{
+void Camera::set_mode(Mode mode) {
     m_impl->set_mode(mode);
 }
 
-void
-Camera::update_self(unsigned long elapsed)
-{
+void Camera::update_self(unsigned long elapsed) {
     m_impl->update_self(elapsed);
 }
 
-void
-Camera::draw_self()
-{
+void Camera::draw_self() {
     m_impl->draw_self();
 }
 
-void
-Camera::follow(const Object* object)
-{
+void Camera::follow(const Object * object) {
     m_impl->follow(object);
 }
 
-void
-Camera::set_limits(const Rect& limits)
-{
+void Camera::set_limits(const Rect& limits) {
     m_impl->set_limits(limits);
 }
