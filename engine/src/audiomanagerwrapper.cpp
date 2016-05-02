307252e7eb8c6a770b01b0e9a9200ed5c95584c1
Dib <ceciliabessa@hotmail.com>
2016-05-02 10:35:33 -0300

    Identation and spacing in AudioManagerWrapper class

--- engine/src/audiomanagerwrapper.cpp
+++ engine/src/audiomanagerwrapper.cpp
@@ -14,60 +14,45 @@ using std::cout;
 using std::endl;
 
 AudioManagerWrapper::AudioManagerWrapper()
-    : sfx_manager{nullptr}, music_manager{nullptr}
-{
+    : sfx_manager{nullptr}, music_manager{nullptr} {
     open_audio();
 }
 
-AudioManagerWrapper::~AudioManagerWrapper()
-{
+AudioManagerWrapper::~AudioManagerWrapper() {
     close_audio();
     delete music_manager;
     delete sfx_manager;
 }
 
-void
-AudioManagerWrapper::init() throw (Exception)
-{
+void AudioManagerWrapper::init() throw (Exception) {
     sfx_manager = new AudioManagerSfx();
 
-    if (not sfx_manager)
-    {
+    if (not sfx_manager) {
         throw Exception("Out of memory for a new AudioManagerSfx");
     }
 
     music_manager = new AudioManagerMusic();
 
-
-    if (not music_manager)
-    {
+    if (not music_manager) {
         throw Exception("Out of memory for a new AudioManagerMusic");
     }
 }
 
-AudioManagerSfx *
-AudioManagerWrapper::sfx() const
-{
+AudioManagerSfx * AudioManagerWrapper::sfx() const {
     return sfx_manager;
 }
 
-AudioManagerMusic *
-AudioManagerWrapper::music() const
-{
+AudioManagerMusic * AudioManagerWrapper::music() const {
     return music_manager;
 }
 
-void
-AudioManagerWrapper::open_audio()
-{
+void AudioManagerWrapper::open_audio() {
     SDL_InitSubSystem(SDL_INIT_AUDIO);
     Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 4096);
     Mix_AllocateChannels(4000);
 }
 
-void
-AudioManagerWrapper::close_audio()
-{
+void AudioManagerWrapper::close_audio() {
     // Query the specifications of the opened audio, to figure out how many times we need to
     // close the audio.
     int frequency = 0;
@@ -76,8 +61,7 @@ AudioManagerWrapper::close_audio()
     const int k_numbers_of_times_opened = Mix_QuerySpec(&frequency, &format, &channels);
     int times_to_close = k_numbers_of_times_opened;
 
-    if (k_numbers_of_times_opened == 0)
-    {
+    if (k_numbers_of_times_opened == 0) {
         cout << "Error querying the specifications of the audio. " << Mix_GetError() << endl;
 
         // Set the amount of times to close the audio to one, just for safety.
@@ -85,8 +69,7 @@ AudioManagerWrapper::close_audio()
     }
 
     // Close the audio the necessary amount of times.
-    while (times_to_close > 0)
-    {
+    while (times_to_close > 0) {
         Mix_CloseAudio();
         times_to_close--;
     }
@@ -94,8 +77,7 @@ AudioManagerWrapper::close_audio()
     // "Since each call to Mix_Init may set different flags, there is no way, currently, to
     // request how many times each one was initted. In other words, the only way to quit for
     // sure is to do a loop like so: "
-    while (Mix_Init(0))
-    {
+    while (Mix_Init(0)) {
         Mix_Quit();
     }
 
