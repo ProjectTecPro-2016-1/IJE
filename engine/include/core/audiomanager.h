1a221a477439c2d186d2a2388fe0c1648ae258bc
Dib <ceciliabessa@hotmail.com>
2016-05-02 11:00:33 -0300

    Identation and spacing in AudioManager.h class

--- engine/include/core/audiomanager.h
+++ engine/include/core/audiomanager.h
@@ -1,10 +1,9 @@
-/*
- * Classe que representa o sistema de audio.
- *
- * Autor: Caio Nardelli
- * Data: 17/05/2015
- * Licença: LGPL. Sem copyright.
- */
+
+ // Class representing the audio system
+ // Author: Caio Nardelli
+ // Date: 17/05/2015
+ // Licence: LGPL. Sem copyright
+
 #ifndef AUDIOMANAGER_H
 #define AUDIOMANAGER_H
 
@@ -14,15 +13,14 @@ using std::string;
 
 static constexpr int k_infinite_loop = -1;
 
-enum class AudioType
-{
+enum class AudioType {
     Music,
     SoundEffect
 };
 
 template <AudioType type>
-class AudioManager final
-{
+class AudioManager final{
+
 public:
     AudioManager();
 
