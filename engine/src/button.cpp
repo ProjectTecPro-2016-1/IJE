4f018b03256eeca81d6d5a365b2d855fd6259d91
Dib <ceciliabessa@hotmail.com>
2016-05-02 10:42:44 -0300

    Identation and spacing in Button class

--- engine/src/button.cpp
+++ engine/src/button.cpp
@@ -16,26 +16,23 @@
 
 #include <cstdio>
 
-class Button::Impl
-{
+class Button::Impl {
 public:
     typedef enum { IDLE, ACTIVE } State;
 
-    Impl(Button *button)
+    Impl(Button * button)
         : m_button(button), m_text(nullptr), m_idle(Color::GREEN),
         m_active(Color::BLUE), m_border(Color::BLACK), m_thickness(1),
-        m_state(IDLE), m_idle_texture(nullptr), m_active_texture(nullptr)
-    {
+        m_state(IDLE), m_idle_texture(nullptr), m_active_texture(nullptr) {
     }
 
-    Impl(Button *button, const string& idle_image_id,
+    Impl(Button * button, const string& idle_image_id,
         const string& active_image_id)
         : m_button(button), m_text(nullptr), m_idle(Color::GREEN),
         m_active(Color::BLUE), m_border(Color::BLACK), m_thickness(1),
-        m_state(IDLE)
-    {
-        Environment *env = Environment::get_instance();
-        
+        m_state(IDLE) {
+        Environment * env = Environment::get_instance();
+
         m_idle_texture = env->resources_manager->get_texture(idle_image_id);
         m_active_texture = env->resources_manager->get_texture(active_image_id);
 
@@ -44,40 +41,33 @@ public:
     }
 
 
-    ~Impl()
-    {
+    ~Impl() {
         delete m_text;
     }
 
-    void set_text(const string& text, const Color& color)
-    {
-        if (text.size() > 0)
-        {
+    void set_text(const string& text, const Color& color) {
+        if (text.size() > 0) {
             delete m_text;
             m_text = new Text(m_button, text, color);
         }
     }
 
-    void set_color(const Color& idle, const Color& active)
-    {
+    void set_color(const Color& idle, const Color& active) {
         m_idle = idle;
         m_active = active;
     }
 
-    void set_border(int thickness, const Color& color)
-    {
+    void set_border(int thickness, const Color& color) {
         m_border = color;
         m_thickness = thickness;
     }
 
-    bool on_event(const MouseButtonEvent& event)
-    {
+    bool on_event(const MouseButtonEvent& event) {
         if (event.state() == MouseButtonEvent::PRESSED and
             event.button() == MouseButtonEvent::LEFT and
-            m_button->bounding_box().contains(event.x(), event.y()))
-        {
+            m_button->bounding_box().contains(event.x(), event.y())) {
             char coords[64];
-            sprintf(coords, "%.2f,%.2f", event.x(), event.y());
+            sprintf(coords, "%.2f, %.2f", event.x(), event.y());
             m_button->notify(clickedID, coords);
 
             return true;
@@ -86,24 +76,19 @@ public:
         return false;
     }
 
-    bool on_event(const MouseMotionEvent& event)
-    {
-        if (m_button->bounding_box().contains(event.x(), event.y()))
-        {
+    bool on_event(const MouseMotionEvent& event) {
+        if (m_button->bounding_box().contains(event.x(), event.y())) {
             m_state = ACTIVE;
 
-            if (m_active_texture.get())
-            {
+            if (m_active_texture.get()) {
                 m_button->set_dimensions(m_active_texture->w(), m_active_texture->h());
             }
 
             return true;
-        } else
-        {
+        } else  {
             m_state = IDLE;
 
-            if (m_idle_texture.get())
-            {
+            if (m_idle_texture.get()) {
                 m_button->set_dimensions(m_idle_texture->w(), m_idle_texture->h());
             }
         }
@@ -111,14 +96,12 @@ public:
         return false;
     }
 
-    void draw_self()
-    {
-        Environment *env = Environment::get_instance();
+    void draw_self() {
+        Environment * env = Environment::get_instance();
         shared_ptr<Texture> image;
         Color color;
 
-        switch (m_state)
-        {
+        switch (m_state) {
         case IDLE:
             image = m_idle_texture;
             color = m_idle;
@@ -130,18 +113,14 @@ public:
             break;
         }
 
-        if (image.get())
-        {
+        if (image.get()) {
             env->canvas->draw(image.get(), m_button->x(), m_button->y());
-        }
-        else
-        {
+        } else {
             env->canvas->set_blend_mode(Canvas::BLEND);
 
             Rect r = m_button->bounding_box();
 
-            for (int i = 0; i < m_thickness; ++i)
-            {
+            for (int i = 0; i < m_thickness; ++i) {
                 env->canvas->draw(r, m_border);
 
                 r.set_position(r.x() + 1, r.y() + 1);
@@ -152,16 +131,15 @@ public:
             env->canvas->set_blend_mode(Canvas::NONE);
         }
 
-        if (m_text)
-        {
+        if (m_text) {
             m_text->align_to(m_button, Object::CENTER, Object::MIDDLE);
             m_text->draw();
         }
     }
 
 private:
-    Button *m_button;
-    Text *m_text;
+    Button * m_button;
+    Text * m_text;
     Color m_idle, m_active, m_border;
     int m_thickness;
     State m_state;
@@ -170,59 +148,44 @@ private:
 
 ActionID Button::clickedID = "clicked()";
 
-Button::Button(Object *parent, ObjectID id, double w, double h)
-    : Object(parent, id, 0, 0, w, h), m_impl(new Impl(this))
-{
-    Environment *env = Environment::get_instance();
+Button::Button(Object * parent, ObjectID id, double w, double h)
+    : Object(parent, id, 0, 0, w, h), m_impl(new Impl(this)) {
+    Environment * env = Environment::get_instance();
     env->events_manager->register_listener(this);
 }
 
-Button::Button(Object *parent, ObjectID id, const string& idle_image_id,
+Button::Button(Object * parent, ObjectID id, const string& idle_image_id,
     const string& active_image_id)
-    : Object(parent, id), m_impl(new Impl(this, idle_image_id, active_image_id))
-{
-    Environment *env = Environment::get_instance();
+    : Object(parent, id), m_impl(new Impl(this, idle_image_id, active_image_id)) {
+    Environment * env = Environment::get_instance();
     env->events_manager->register_listener(this);
 }
 
-Button::~Button()
-{
-    Environment *env = Environment::get_instance();
+Button::~Button() {
+    Environment * env = Environment::get_instance();
     env->events_manager->unregister_listener(this);
 }
 
-void
-Button::draw_self()
-{
+void Button::draw_self() {
     m_impl->draw_self();
 }
 
-bool
-Button::on_event(const MouseButtonEvent& event)
-{
+bool Button::on_event(const MouseButtonEvent& event) {
     return m_impl->on_event(event);
 }
 
-bool
-Button::on_event(const MouseMotionEvent& event)
-{
+bool Button::on_event(const MouseMotionEvent& event) {
     return m_impl->on_event(event);
 }
 
-void
-Button::set_text(const string& text, const Color& color)
-{
+void Button::set_text(const string& text, const Color& color) {
     return m_impl->set_text(text, color);
 }
 
-void
-Button::set_color(const Color& idle, const Color& active)
-{
+void Button::set_color(const Color& idle, const Color& active) {
     return m_impl->set_color(idle, active);
 }
 
-void
-Button::set_border(int thickness, const Color& color)
-{
+void Button::set_border(int thickness, const Color& color) {
     return m_impl->set_border(thickness, color);
 }
