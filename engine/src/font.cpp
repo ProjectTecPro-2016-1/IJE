76d17f3d57b2624f7562344fa25949daf3e7bbd2
carolinaferreira <carol.mf14@gmail.com>
2016-05-30 01:23:20 -0300

    description of functions were adjusted

--- engine/src/font.cpp
+++ engine/src/font.cpp
@@ -4,28 +4,67 @@
 
 class Font::Impl {
     public:
+                
+        // -------------------------------------------------------------  
+        // Function: Impl()  
+        // Description: Implements a loaded font of a file.
+        // Parameters:
+        //      TTF_Font *font;         Font from file
+        //      const string& path;     
+        //      int size;               Font size
+        // -------------------------------------------------------------   
         Impl(TTF_Font *font, const string& path, int size)
             : m_font(font), m_path(path), m_size(size), m_style(NORMAL) {
         }
 
+
+        // -------------------------------------------------------------  
+        // Function: ~Impl()
+        // Description: Destructor of Impl class.
+        // -------------------------------------------------------------   
         ~Impl() {
             if (m_font) {
                 TTF_CloseFont(m_font);
             }
         }
 
+        // -------------------------------------------------------------  
+        // Function: font()  
+        // Description: Returns the font file.
+        // Return: m_font
+        // -------------------------------------------------------------   
         TTF_Font * font() const {
             return m_font;
         }
 
+        
+        // -------------------------------------------------------------  
+        // Function: size()  
+        // Description: Returns the font size.
+        // Return: m_size
+        // -------------------------------------------------------------   
         int size() const {
             return m_size;
         }
 
+        
+        // -------------------------------------------------------------  
+        // Function: style()  
+        // Description: Returns the font style.
+        // Return: m_style
+        // -------------------------------------------------------------   
         Style style() const {
             return m_style;
         }
 
+        
+        // -------------------------------------------------------------  
+        // Function: set_size()  
+        // Description: Set the font size.
+        // Parameters:
+        //      int size;         Escrever aqui
+        // Return: void
+        // -------------------------------------------------------------   
         void set_size(int size) {
             Environment *env = Environment::get_instance();
             shared_ptr<Settings> settings = env->resources_manager->get_settings(env->m_settings_path);
@@ -34,6 +73,15 @@ class Font::Impl {
             change_size(size * k);
         }
 
+        
+        // -------------------------------------------------------------  
+        // Function: set_style()  
+        // Description: Set the font style.
+        // Parameters:
+        //      Style style;         Type of font, like bold, italic or
+        //                           normal.
+        // Return: void
+        // -------------------------------------------------------------   
         void set_style(Style style) {
             switch (style) {
                 case BOLD:
@@ -56,6 +104,13 @@ class Font::Impl {
         int m_size;
         Style m_style;
 
+        // -------------------------------------------------------------  
+        // Function: change_size()  
+        // Description: Change the font size.
+        // Parameters:
+        //      int size;         Font size.
+        // Return: void
+        // -------------------------------------------------------------   
         void change_size(int size) {
             if (m_size == size) {
                 return;
@@ -73,13 +128,34 @@ class Font::Impl {
         }
 };
 
+// -------------------------------------------------------------  
+// Function: Font()  
+// Description: Implements font according the attributes.
+// Parameters:
+//      TTF_Font *font;         Font from file
+//      const string& path;             
+//      int size                Font size
+// -------------------------------------------------------------   
 Font::Font(TTF_Font *font, const string& path, int size)
     : m_impl(new Font::Impl(font, path, size)) {
 }
 
+
+// -------------------------------------------------------------  
+// Function: ~Font()  
+// Observations:
+//      Funtion not implemented        
+// -------------------------------------------------------------   
 Font::~Font() {
 }
 
+// -------------------------------------------------------------  
+// Function: from_file()  
+// Description: Get the font from file.
+// Parameters:
+//      const string& path;     
+// Return: Font *
+// -------------------------------------------------------------   
 Font * Font::from_file(const string& path) throw (Exception) {
     TTF_Font *font = TTF_OpenFont(path.c_str(), 20);
 
@@ -90,22 +166,51 @@ Font * Font::from_file(const string& path) throw (Exception) {
     return new Font(font, path);
 }
 
+// -------------------------------------------------------------  
+// Function: font()
+// Description: Implements a font.
+// Return: m_impl->font
+// -------------------------------------------------------------   
 TTF_Font * Font::font() const {
     return m_impl->font();
 }
 
+// -------------------------------------------------------------  
+// Function: size()  
+// Description: Returns the font size.
+// Return: m_impl->size
+// -------------------------------------------------------------   
 int Font::size() const {
     return m_impl->size();
 }
 
+// -------------------------------------------------------------  
+// Function: style()  
+// Description: Returns the font style.
+// Return: m_impl->style
+// -------------------------------------------------------------   
 Font::Style Font::style() const {
     return m_impl->style();
 }
 
+// -------------------------------------------------------------  
+// Function: set_size()  
+// Description: Set the font size.
+// Parameters:
+//      int size;     Font size
+// Return: void
+// -------------------------------------------------------------   
 void Font::set_size(int size) {
     m_impl->set_size(size);
 }
 
+// -------------------------------------------------------------  
+// Function: set_style()  
+// Description: Set the font style.
+//      Style style;         Type of font, like bold, italic or
+//                           normal.    
+// Return: void
+// -------------------------------------------------------------   
 void Font::set_style(Style style) {
     m_impl->set_style(style);
 }
